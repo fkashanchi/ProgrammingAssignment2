ee1d711742afb8c99f51e6e11b4667b5405388df
fkashanchi <45673541+fkashanchi@users.noreply.github.com>
2019-01-06 20:57:49 -0800

    Programming Assignment 2
    Programming Assignment 2 Submission

--- ProgrammingAssignment2.r
+++ ProgrammingAssignment2.r
@@ -1,16 +1,7 @@
-## Functions for creating and using inverted matrices whith caching ability 
-
- 
-
- 
-## Creates cacheable matrix for inputting to 
-## cacheSolve() function which sets and gets  
-## the cached values 
-
+##makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
  
 makeCacheMatrix <- function(original.matrix = matrix()) { 
  
- # Let's check if we have correct input 
  if (!is.matrix(original.matrix)) { 
     stop("Please give a matrix") 
   } 
@@ -21,9 +12,8 @@ makeCacheMatrix <- function(original.matrix = matrix()) {
      original.matrix <<- y 
     inverted.matrix <<- NULL 
   } 
-      # Functions for getting and setting cached inv. matrix value 
+
    get <- function() original.matrix 
-  # Inversing the matrix using build in solve() function in R 
   set.inverse <- function(solve) inverted.matrix <<- solve 
    get.inverse <- function() inverted.matrix 
     
@@ -35,23 +25,16 @@ makeCacheMatrix <- function(original.matrix = matrix()) {
     
  } 
  
- 
+##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
- 
-## Computes the inverse of the cacheable matrix returned by makeCacheMatrix() 
-## If the inverse has already been calculated and there's no change in the matrix 
-## then the cacheSolve() returns the cached inverse 
-
- 
 cacheSolve <- function(cacheable.matrix, ...) { 
-  inverted.matrix <- cacheable.matrix$get.inverse() 
-  # Do we have cached matrix available? 
+inverted.matrix <- cacheable.matrix$get.inverse() 
+  
   if(!is.null(inverted.matrix)) { 
     message("Getting cached inverse matrix") 
      return(inverted.matrix) 
    } 
-  # Let's create inverted matrix in case 
-   # there's no cached matrix available. 
+ 
    matrix.to.inverse <- cacheable.matrix$get() 
    inverted.matrix <- solve(matrix.to.inverse) 
   cacheable.matrix$set.inverse(inverted.matrix) 
